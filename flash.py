6c635cb225a7d636d1738beb93a2e20a7a64dcdf
formazione <gatto.gio@gmail.com>
2020-09-13 11:27:23 +0200

    version 1.5 and engine wip

--- flash.py
+++ flash.py
@@ -16,14 +16,15 @@ clock = pygame.time.Clock()
 
 
 
-def load_images(folder):
-    # This has things to overlay onto layer 1
+def load_images(folder: str) -> list:
+    "Load tiles from a folder... with a number at the end"
     listtiles2 = [x for x in glob(folder + "2\\*.png")]
     tile2 = [pygame.image.load(x) for x in listtiles2]
     return tile2
 
 
-def load_map(filename, mp1):
+def load_map(filename: str, mp1: list):
+    "Resume a list with the data (letter) for the tiles to be displayed on display surface"
     if filename in os.listdir():
         with open(filename, "rb") as file:
             mp = pickle.load(file)
@@ -33,30 +34,26 @@ def load_map(filename, mp1):
 
 
 def showmap(mp1):
+    "Take the map list with letters and blit them as tiles on the display surface"
     for y, line in enumerate(mp1):
         for x, c in enumerate(line):
             for n, l in enumerate(letters):
                 if c == l:
                     display.blit(tile[n], (x * 16, y * 16))
 
-# def map_to_list():
-#     "Creates a list of 16 lists with 29 empty spaces as items"
-#     map1 = []
-#     for x in range(16):
-#         line = [x for x in "                             "]
-#         map1.append(line)
-#     return map1
 
+# calls the function to load tiles from imgs2/
 tile = load_images("imgs")
+# create a list of all letters corrisponding to the tiles with images from imgs2 folder
 alphab = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm.,:;@#°^[]<>()&%$£€ABC1234567890òàèéù+-ì={}§!?/|"
 letters = [x for x in alphab[0:len(tile)]]
 map2 = []
+# Stores a list with letters = tiles from pkl file (created with pygame map editor)
 map2 = load_map("last_map2.pkl", map2)
 
-# print(map2)
-# showmap(map2)
 
 loop = 1
+# This blit all the tiles on the display surface
 showmap(map2)
 while loop:
     for event in pygame.event.get():
@@ -65,7 +62,7 @@ while loop:
         if event.type == pygame.KEYDOWN:
             if event.key == K_ESCAPE:
                 loop = 0
-
+    # the display surface is scaled as the screen (being the half of screen size)
     screen.blit(pygame.transform.scale(display, WSIZE), (0, 0))
     pygame.display.update()
     clock.tick(60)
